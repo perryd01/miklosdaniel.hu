8e5577ad6927a9ba96b09ce2cd155d843d1e57dc
Miklos Daniel <daniel.miklos0@gmail.com>
2022-07-17 21:24:59 +0200

    projects are clickable
    Signed-off-by: Miklos Daniel <daniel.miklos0@gmail.com>

--- src/lib/components/project/ProjectPreview.svelte
+++ src/lib/components/project/ProjectPreview.svelte
@@ -3,15 +3,17 @@
 	import Title from '../content/Title.svelte';
 
 	export let project: IProject;
-	const { date, link, tags, text, title } = project;
+	const { link, tags, text, title } = project;
 </script>
 
-<div class="p-4 rounded-xl bg-app-secondary text-white">
-	<Title heading={3}>{title}</Title>
-	<div class="mb-2 flex flex-row gap-4 items-center justify-start">
-		{#each tags as tag}
-			<span class="px-2 py-[2px] bg-app-yellow text-black rounded-xl">{tag}</span>
-		{/each}
+<a href={link} target="_blank" rel="noreferrer">
+	<div class="p-4 rounded-xl bg-app-secondary text-white">
+		<Title heading={3}>{title}</Title>
+		<div class="mb-2 flex flex-row gap-4 items-center justify-start">
+			{#each tags as tag}
+				<span class="px-2 py-[2px] bg-app-yellow text-black rounded-xl">{tag}</span>
+			{/each}
+		</div>
+		<p>{text}</p>
 	</div>
-	<p>{text}</p>
-</div>
+</a>
