34414313aa01ed46b4fb854a39cf3b484650ded0
Miklos Daniel <daniel.miklos0@gmail.com>
2022-07-31 17:45:46 +0200

    fix sitemap namespace

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -11,7 +11,7 @@ export async function GET() {
 		},
 		body: `
         <?xml version="1.0" encoding="UTF-8" ?>
-        <urlset>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             <url>
                 <loc>${host}/</loc>
             </url>
