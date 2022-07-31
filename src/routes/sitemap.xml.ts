599597b42b766bc1fde0860b7f36e876b23f490b
Miklos Daniel <daniel.miklos0@gmail.com>
2022-07-31 17:40:09 +0200

    update sitemap

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -1,6 +1,10 @@
+import { getAlbums } from '$lib/contentfulClient';
+
 const host = 'https://miklosdaniel.hu';
 
 export async function GET() {
+	const albums = await getAlbums();
+	const links: string[] = albums.items.map((album) => album.fields.slug);
 	return {
 		headers: {
 			'Content-Type': 'application/xml'
@@ -17,6 +21,11 @@ export async function GET() {
             <url>
                 <loc>${host}/coding</loc>
             </url>
+            ${links
+							.map((l) => {
+								return `<url><loc>${host}/photography/${l}</loc></url>`;
+							})
+							.join('')}
         </urlset>
         `.trim()
 	};
