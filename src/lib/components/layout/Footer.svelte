a242270242b87652aee3dcf09da503efd1f7609a
Miklos Daniel <daniel.miklos0@gmail.com>
2024-04-22 15:04:49 +0200

    feat: add footer

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,7 +1,30 @@
-<footer>m</footer>
+<script lang="ts">
+	const bt = new Date(__BUILDTIME__);
+
+	const [month, day, hour, minute] = [
+		bt.getMonth() + 1,
+		bt.getDate(),
+		bt.getHours(),
+		bt.getMinutes()
+	].map(padNumber);
+
+	function padNumber(n: number) {
+		return String(n).padStart(2, '0');
+	}
+</script>
+
+<!-- svelte-ignore missing-declaration -->
+<footer>
+	<p>
+		v{__PACKAGE__.version}
+	</p>
+	<p class="text-xs">
+		{`${String(bt.getFullYear()).slice(2)}${[month, day, hour, minute].join('')}`}
+	</p>
+</footer>
 
 <style lang="postcss">
 	footer {
-		@apply bg-black;
+		@apply max-w-screen-lg mx-auto w-full;
 	}
 </style>
