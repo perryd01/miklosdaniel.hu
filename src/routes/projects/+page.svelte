d377269e572dac9b54a26cbb2fc8faed6f9fa327
Miklos Daniel <daniel.miklos0@gmail.com>
2024-05-04 13:34:54 +0200

    fix: unused imports

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-	import { formatDate } from '$lib/utils';
-	import { Dot } from 'lucide-svelte';
 	import type { PageData } from './$types';
-	import TechnologyTag from '$lib/components/projects/TechnologyTag.svelte';
 	import ListProjects from '$lib/components/projects/ListProjects.svelte';
 	export let data: PageData;
 </script>
