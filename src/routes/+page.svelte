4f51838c44b48c2092ae04189c0b6fcac2b296ad
Miklos Daniel <daniel.miklos0@gmail.com>
2024-05-04 11:41:47 +0200

    refactor: create project listing component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
 	import Grid from '$lib/components/grid';
+	import ListProjects from '$lib/components/projects/ListProjects.svelte';
 
 	import * as config from '$lib/config';
+	import { onMount } from 'svelte';
+	import { fade, slide } from 'svelte/transition';
 
 	export let data;
 
-	const projects = data.projects.sort((a, b) => {
-		if (a.order < b.order) return -1;
-		if (a.order > b.order) return 1;
-		return 0;
-	});
+	let animateFlag = false;
+
+	onMount(() => (animateFlag = true));
 </script>
 
 <svelte:head>
 	<title>{config.title}</title>
 </svelte:head>
+
+<div>
+	<section>
+		<h1 class="text-5xl font-bold">Hi, I'm Miklós</h1>
+		<p>a fullstack developer, recently graduated at BUTE</p>
+	</section>
+
+	<ListProjects compact projects={data.projects.filter((x) => x.important)}>
+		<h2 slot="title" class="text-4xl font-medium">Recent Projects</h2>
+		<a slot="nav-button" href="/projects" class="text-xl font-medium">View all my projects</a>
+	</ListProjects>
+</div>
+
+<style lang="postcss">
+	div {
+		@apply grid gap-8;
+	}
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,6 +3,7 @@
 	import { Dot } from 'lucide-svelte';
 	import type { PageData } from './$types';
 	import TechnologyTag from '$lib/components/projects/TechnologyTag.svelte';
+	import ListProjects from '$lib/components/projects/ListProjects.svelte';
 	export let data: PageData;
 </script>
 
@@ -10,49 +11,4 @@
 	<title>Projects</title>
 </svelte:head>
 
-<section>
-	<ul class="projects">
-		{#each data.projects as project}
-			<li class="project">
-				<a href={`/projects/${project.slug}`} class="title">{project.title}</a>
-
-				<div class="flex flex-row items-center date">
-					<p>{project.readingTime.text}</p>
-					<Dot />
-					<p>Published at {formatDate(project.date)}</p>
-				</div>
-				<div class="tags">
-					{#each project.categories || [] as tag}
-						<TechnologyTag {tag} />
-					{/each}
-				</div>
-				<p class="description">{project.description}</p>
-			</li>
-		{/each}
-	</ul>
-</section>
-
-<style lang="postcss">
-	.projects {
-		@apply grid gap-8;
-	}
-
-	.title {
-		@apply text-3xl capitalize;
-	}
-
-	.date {
-		@apply text-lg text-gray-200;
-	}
-
-	.description {
-		@apply mt-2;
-	}
-
-	.tags {
-		@apply flex flex-wrap gap-2;
-	}
-	.tag {
-		@apply p-1 bg-gray-800 text-gray-200 rounded-md;
-	}
-</style>
+<ListProjects projects={data.projects} />
