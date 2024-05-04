fa9c8228cc7c75a5c3981b30fe2e9dd9aa11daba
Miklos Daniel <daniel.miklos0@gmail.com>
2024-05-04 13:35:23 +0200

    fix: add project description meta tag

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -12,6 +12,8 @@
 	<title>{data.meta.title}</title>
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content={data.meta.title} />
+	<meta property="og:description" content={data.meta.description} />
+	<meta property="og:type" content="blog" />
 </svelte:head>
 
 <article class="prz">
